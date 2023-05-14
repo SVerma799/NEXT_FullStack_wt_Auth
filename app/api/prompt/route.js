d7245c3890bd11117a746352cadb22e9ded57766
Shubham Verma <vshubham799@gmail.com>
2023-05-14 01:35:55 -0700

    Changes done to add the Promptcard render

---
+++ app/api/prompt/route.js
@@ -0,0 +1,17 @@
+import { connectDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (req, res) => {
+  try {
+    await connectDB();
+    const prompts = await Prompt.find({}).populate("creator");
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+    });
+  } catch (err) {
+    console.log(err);
+    return new Response("Failed to fetch prompts", {
+      status: 500,
+    });
+  }
+};
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+"use client";
+
+import { useEffect, useState } from "react";
+import PromptCard from "./PromptCard";
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post, idx) => (
+        <div key={idx}>
+          <PromptCard
+            key={post.id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        </div> // this is the prompt card
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [search, setSearch] = useState("");
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/prompt");
+      const data = await res.json();
+      console.log(data);
+      setPosts(data);
+    };
 
-export default Feed
+    fetchData();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or username"
+          className="search_input"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          required
+        />
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
